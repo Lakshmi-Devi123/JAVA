febd548b4562de3afd4bc3812d80130a4972bd4b
Lakshmi Devi Surakarapu <154741944+Lakshmi-Devi123@users.noreply.github.com>
2025-10-12 10:49:03 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+package EvenorOdd;
+import java.util.*;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if (num%2==0) {
+			System.out.println("The given number is Even Number.");
+		}
+		else {
+			System.out.println("The given numbe is Odd Number.");
+		}
+
+	}
+
+}
