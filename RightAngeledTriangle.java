b73c670ea5ca549da1e559874d11ee47226cc0ce
Lakshmi Devi Surakarapu <154741944+Lakshmi-Devi123@users.noreply.github.com>
2025-10-15 09:46:59 +0530

    Add files via upload

---
+++ RightAngeledTriangle.java
@@ -0,0 +1,21 @@
+package rightangeledtriangle;
+import java.util.*;
+
+public class RightAngeledTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+		}
+
+	}
+
+}
---
+++ SquarePattern.java
@@ -0,0 +1,20 @@
+package squarepattern;
+import java.util.*;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the Number: ");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
